100e9c1a02623f15871dad8e8e7871a657a2475d
thabullfrog <th3bullfrog@gmail.com>
2017-04-17 17:59:37 -0400

    add domain name

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@
 
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
-server "45.56.91.160", user: "deploy", roles: %w{app db web}
+server "bullfrogwebsites.com", user: "deploy", roles: %w{app db web}
 
 
 
