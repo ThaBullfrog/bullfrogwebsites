7239a148f66606a7f295e406fda8445b789d2f83
thabullfrog <th3bullfrog@gmail.com>
2017-04-17 17:42:23 -0400

    apply another small fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@
 
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
-server "127.0.0.1", user: "deploy", roles: %w{app db web}
+server "45.56.91.160", user: "deploy", roles: %w{app db web}
 
 
 
