1cc8dbd1a66a77c0364c0e4aa5bc823a8f60e313
thabullfrog <th3bullfrog@gmail.com>
2017-04-17 22:10:37 -0400

    add home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def full_title(page_title='')
+    base_title = "Bullfrog Websites"
+    if page_title.empty?
+      return base_title
+    else
+      return page_title + " | " + base_title
+    end
+  end
+
+  def show_flashes
+    returns = Array.new
+    flash.each do |message_type, message|
+      returns.push(("<div class=\"alert alert-" + message_type + "\">" + message + "</div>"))
+    end
+    return returns.join.html_safe
+  end
+
+  def top(color: "white")
+    render(partial: 'shared/top', locals: { color: color })
+  end
 end
