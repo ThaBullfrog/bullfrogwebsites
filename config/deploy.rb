057cf295a694a0cd42364b3162ce3e8be793db47
Jeremiah Kellick <jeremiahkellick@icloud.com>
2017-04-26 15:17:13 +0000

    change git repo url to https version

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.8.0"
 
 set :application, "bullfrogwebsites"
-set :repo_url, "git@github.com:ThaBullfrog/bullfrogwebsites.git"
+set :repo_url, "https://github.com/ThaBullfrog/bullfrogwebsites"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
