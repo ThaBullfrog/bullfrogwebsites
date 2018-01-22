418947ee009867e208c3c21f7ff41dfd5fe3bd9a
ThaBullfrog <th3bullfrog@gmail.com>
2018-01-22 15:35:40 -0500

    fix email in test

--- test/mailers/quote_request_mailer_test.rb
+++ test/mailers/quote_request_mailer_test.rb
@@ -6,7 +6,7 @@ class QuoteRequestMailerTest < ActionMailer::TestCase
     request = QuoteRequest.new(name: "Test Name", email: "test@example.com", description: "test message")
     mail = QuoteRequestMailer.quote_request(request)
     assert_equal "QUOTE REQUEST", mail.subject
-    assert_equal ["jeremiahkellick@icloud.com"], mail.to
+    assert_equal ["jeremiahkellick@gmail.com"], mail.to
     assert_equal [request.email], mail.from
     assert_match request.description, mail.body.encoded
     assert_match request.name, mail.body.encoded
